217bacec81315a31a8a514b8faf439bf30e7054e
X301920 <gabriel.pinheiro.campos@prservicos.com.br>
2024-08-23 11:39:49 -0300

    initial commit

---
+++ src/main/java/com/orionsolution/oauthsecurity/OauthsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.orionsolution.oauthsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthsecurityApplication.class, args);
+	}
+
+}

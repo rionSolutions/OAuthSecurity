c9284502b903ad035775eac1891b61c667494c3d
Gabriel Campos <gabrielcampos.dgr@hotmail.com>
2024-08-31 00:40:17 -0300

    AJUSTE pastas

--- src/main/java/com/orionsolution/oauthsecurity/controller/AuthController.java
+++ src/main/java/com/orionsolution/oauthsecurity/controller/AuthController.java
@@ -19,11 +19,21 @@ public class AuthController {
         this.oauthService = oauthService;
     }
 
+    /**
+     * @param requireSessionDTO
+     * @return AuthorizationDTO
+     * @Note Application request authorization for validate your credentials
+     */
     @PostMapping("/requestAuthorization")
     public ResponseEntity<AuthorizationDTO> requestAuthorization(@RequestBody RequireSessionDTO requireSessionDTO) {
         return ResponseEntity.ok(oauthService.requestAuthorization(requireSessionDTO));
     }
 
+    /**
+     * @param requireSessionDTO
+     * @return AuthorizationDTO
+     * @Note Application request access session for validate if the credentials received is secure and turn the session active
+     */
     @PostMapping("/requestAccessSession")
     public ResponseEntity<AuthorizationDTO> registerAccessSession(@RequestBody RequireSessionDTO requireSessionDTO) {
         return ResponseEntity.ok(oauthService.registerApplicationSession(requireSessionDTO));
